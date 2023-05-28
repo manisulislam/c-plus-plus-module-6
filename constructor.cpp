1fd3e81923bd205df5d88fecdf2e75c7b8bca584
anisul islam <anisulislam980340@gmail.com>
2023-05-29 05:49:17 +0600

    string constructor completed

---
+++ constructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //string s="hello world";
+    //using constructor
+    //string s("hello anisul islam");
+    //string s("hello anisul islam", 4);
+    // string a="hello world";
+    // string s(a, 4);
+    string a(5,'a');
+    cout<<a<<endl;
+    return 0;
+}
